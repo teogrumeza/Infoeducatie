dbe8b3e559ac69b5a433957d9a9ca66658e68c6a
teogrumeza <teo_grumeza@yahoo.com>
2017-07-22 21:26:15 +0300

    Add files via upload

---
+++ ShoesEye_SourceCode.ino
@@ -0,0 +1,42 @@
+const int trigPin = 2;
+const int echoPin = 4;
+
+void setup() {
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  long duration, inches, cm;
+
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  cm = microsecondsToCentimeters(duration);
+  Serial.println(cm);
+  if(cm <= 100) {
+     beep(2 * cm, 31);
+  }
+  
+  delay(100);
+}
+
+void beep(unsigned char delayms, int sound) {
+  analogWrite(9, sound);     
+                           
+  delay(delayms);          
+  analogWrite(9, 0);       
+  delay(delayms);             
+} 
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
+}
